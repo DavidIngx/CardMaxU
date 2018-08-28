1f3b377ca679e42a147dd2ff313085497635e684
David Peña <brian.penam@campusucc.edu.co>
2018-08-28 13:44:01 -0500

    Proyecto Inicial
    Configuraciones iniciales para la correcta ejecucion del proyecto

--- app/src/main/java/com/numa/cardmax/cardmaxu/MainActivity.java
+++ app/src/main/java/com/numa/cardmax/cardmaxu/MainActivity.java
@@ -31,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
