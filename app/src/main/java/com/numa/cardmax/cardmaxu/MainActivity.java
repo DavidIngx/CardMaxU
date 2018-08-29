32a31dd79e3c73adc624d73fa65976980065f23c
David Peña <brian.penam@campusucc.edu.co>
2018-08-29 15:22:20 -0500

    Fragment deTab
    Se genera una version estable para trabajar las vistas de cada fragment

--- app/src/main/java/com/numa/cardmax/cardmaxu/MainActivity.java
+++ app/src/main/java/com/numa/cardmax/cardmaxu/MainActivity.java
@@ -3,31 +3,45 @@ package com.numa.cardmax.cardmaxu;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private FrameLayout mainFrame;
+    private MuroFragment murofrag;
+    private NotificacionFragment notifrag;
+    private PerfilFragment perfilfrag;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    setFrame(perfilfrag);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    setFrame(murofrag);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    setFrame(notifrag);
                     return true;
             }
             return false;
+        }
+
+        private void setFrame(Fragment fragment) {
+            FragmentTransaction transcicion = getSupportFragmentManager().beginTransaction();
+            transcicion.replace(R.id.mainfragment, fragment);
+            transcicion.commit();
         }
     };
 
@@ -40,6 +54,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        murofrag = new MuroFragment();
+        perfilfrag = new PerfilFragment();
+        notifrag = new NotificacionFragment();
+        mainFrame = (FrameLayout) findViewById(R.id.mainfragment);
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
